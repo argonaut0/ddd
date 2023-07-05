76fb7711de80f3d55ffda8d4468c5deea2d3d3d6
Jason Hsu <jasonhsu2013@gmail.com>
2023-07-04 23:31:35 -0700

    Add polling mode

--- README.md
+++ README.md
@@ -5,8 +5,10 @@ On execution creates or updates the IPv4 A record for a specific domain. Errors 
 
 ## Usage
 Set environment variables:
-- `CLOUDFLARE_API_TOKEN` The Cloudflare API token (not API key)
-- `DNS_A_RECORD_FQDN` The FQDN to use in the A record
-- `CLOUDFLARE_SITE_ZONE_ID` The Zone ID for the site
+- `CLOUDFLARE_API_TOKEN` The Cloudflare API token (not API key).
+- `DNS_A_RECORD_FQDN` The FQDN to use in the A record.
+- `CLOUDFLARE_SITE_ZONE_ID` The Zone ID for the site.
+- `POLLING` Set to `true` to continue check for IP changes every predetermined interval.
+- `POLL_INTERVAL_SECONDS` Set polling interval in seconds. Default 120.
 
 Run the binary.
--- cmd/main.go
+++ cmd/main.go
@@ -7,6 +7,7 @@ import (
 	"log"
 	"net/http"
 	"os"
+	"strconv"
 	"time"
 
 	"github.com/cloudflare/cloudflare-go"
@@ -15,7 +16,19 @@ import (
 const PublicIPEchoEndpoint = "https://api.ipify.org"
 
 func retrievePublicIp() (string, error) {
-	res, err := http.Get(PublicIPEchoEndpoint)
+
+	req, err := http.NewRequest("GET", PublicIPEchoEndpoint, nil)
+	if err != nil {
+		return "", err
+	}
+
+	ctx, cancel := context.WithTimeout(req.Context(), 2*time.Second)
+	defer cancel()
+
+	req = req.WithContext(ctx)
+
+	client := http.DefaultClient
+	res, err := client.Do(req)
 	if err != nil {
 		return "", err
 	}
@@ -27,20 +40,13 @@ func retrievePublicIp() (string, error) {
 	return string(ip), nil
 }
 
-func main() {
+func createOrUpdateIP(targetDomain, targetIP, zoneID string) error {
 	api, err := cloudflare.NewWithAPIToken(os.Getenv("CLOUDFLARE_API_TOKEN"))
 	if err != nil {
-		log.Fatalf("Error initializing Cloudflare clint: %s", err)
+		return fmt.Errorf("error initializing Cloudflare client: %s", err)
 	}
-	targetDomain := os.Getenv("DNS_A_RECORD_FQDN")
-	targetIP, err := retrievePublicIp()
-	zoneID := os.Getenv("CLOUDFLARE_SITE_ZONE_ID")
-	if err != nil {
-		log.Fatalf("Could not retrieve public IPv4: %s", err)
-	}
-	log.Printf("Public IP is: %s", targetIP)
-
-	ctx, cancelCtx := context.WithTimeout(context.Background(), 20*time.Second)
+	ctx, cancelCtx := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancelCtx()
 
 	listParams := cloudflare.ListDNSRecordsParams{
 		Type: "A",
@@ -50,10 +56,10 @@ func main() {
 	// Fetch user details on the account
 	l, i, err := api.ListDNSRecords(ctx, cloudflare.ZoneIdentifier(zoneID), listParams)
 	if err != nil {
-		log.Fatalf("Error retrieving existing DNS Records: %s", err)
+		return fmt.Errorf("error retrieving existing DNS Records: %s", err)
 	}
 	if i.Count > 1 {
-		log.Fatalf("More than 1 record for %s exists.", targetDomain)
+		return fmt.Errorf("more than 1 record for %s exists", targetDomain)
 	}
 	if i.Count == 0 {
 		recordCreate := cloudflare.CreateDNSRecordParams{
@@ -65,7 +71,7 @@ func main() {
 		}
 		_, err := api.CreateDNSRecord(ctx, cloudflare.ZoneIdentifier(zoneID), recordCreate)
 		if err != nil {
-			log.Fatalf("Error creating new DNS record: %s", err)
+			return fmt.Errorf("error creating new DNS record: %s", err)
 		}
 		fmt.Println("Successfully created new DNS record")
 	} else {
@@ -79,9 +85,62 @@ func main() {
 		}
 		_, err := api.UpdateDNSRecord(ctx, cloudflare.ZoneIdentifier(zoneID), recordUpdate)
 		if err != nil {
-			log.Fatalf("Error updating DNS record: %s", err)
+			return fmt.Errorf("error updating DNS record: %s", err)
 		}
 		log.Println("Successfully updated DNS record")
 	}
-	cancelCtx()
+	return nil
+}
+
+func main() {
+	poll := os.Getenv("POLLING")
+	pollVar, intervalSpecified := os.LookupEnv("POLL_INTERVAL_SECONDS")
+	var pollInterval = 120
+	if intervalSpecified {
+		var err error
+		pollInterval, err = strconv.Atoi(pollVar)
+		if err != nil {
+			log.Println("Error parsing poll interval, ignoring")
+		}
+	}
+
+	targetDomain := os.Getenv("DNS_A_RECORD_FQDN")
+	targetIP, err := retrievePublicIp()
+	zoneID := os.Getenv("CLOUDFLARE_SITE_ZONE_ID")
+	if err != nil {
+		log.Printf("Could not retrieve public IPv4: %s", err)
+		if poll != "true" {
+			os.Exit(1)
+		}
+	} else {
+		log.Printf("Public IP is: %s", targetIP)
+		err = createOrUpdateIP(targetDomain, targetIP, zoneID)
+		if err != nil {
+			log.Println(err)
+			if poll != "true" {
+				os.Exit(1)
+			}
+		}
+	}
+
+	if poll == "true" {
+		for {
+			time.Sleep(time.Duration(pollInterval) * time.Second)
+			newIP, err := retrievePublicIp()
+			if err != nil {
+				log.Printf("Could not retrieve public IPv4: %s", err)
+			} else {
+				if newIP != targetIP {
+					log.Printf("IP changed from %s to %s, updating...", targetIP, newIP)
+					targetIP = newIP
+					err = createOrUpdateIP(targetDomain, targetIP, zoneID)
+					if err != nil {
+						log.Println(err)
+					}
+				} else {
+					log.Println("Retrieved new IP, no change detected")
+				}
+			}
+		}
+	}
 }
